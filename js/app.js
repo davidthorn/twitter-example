759c3dc8c5845165eac8ded8435d347f232b1815
David Thorn <david.thorn@atino.de>
2018-07-31 07:49:22 +0200

    added app.js loading of templates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
+const loadTemplates = (parent) => {
+    
+    /// Find all tags which have the attribute template
+    const items = parent.querySelectorAll('[template]');
+    
+    /// iterate through all elements which the query has found
+    items.forEach(item => {
+        
+        const template = item.getAttribute('template');
+
+        /// fetch the html content from the server
+        fetch(template)
+        /// handle a successful response
+        .then((response) => {
+            return response.text();
+        })
+        .then(content => {
+            /// assign the html content loaded to 
+            /// the items innerHTML
+            item.innerHTML = content;
+
+            /// run this command on the item element so as to find any child templates
+            loadTemplates(item);
+        })
+        
+   });
+}
+
 const onWindowLoaded = () => {
-    console.log('window loaded');
+    loadTemplates(document);
 }
 
 window.onload = onWindowLoaded;
